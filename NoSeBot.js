3d2d8e031d8dca11bfc840e6c2facade809d9cf1
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-12-17 03:44:01 -0600

    Create NoSeBot.js
    New

---
+++ NoSeBot.js
@@ -0,0 +1,6 @@
+[
+  "No se a que te refieres ¿Puedes Repetirlo?",
+  "No me programaron para eso lo Siento 😢",
+  "Soy una inteligencia muy limitada y no se responderte.",
+  "Solo soy un Chatbot no puedo hacer eso."
+];
