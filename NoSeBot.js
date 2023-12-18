dd5ac6c382378037bcfc4e9130f6eaa2c10338d5
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2023-12-18 13:43:54 -0600

    Update NoSeBot.js

--- NoSeBot.js
+++ NoSeBot.js
@@ -2,5 +2,6 @@
   "No se a que te refieres ¿Puedes Repetirlo?",
   "No me programaron para eso lo Siento 😢",
   "Soy una inteligencia muy limitada y no se responderte.",
-  "Solo soy un Chatbot no puedo hacer eso."
+  "Solo soy un Chatbot no puedo hacer eso.",
+  "Puedes Explicarmelo mejor?"
 ];
