64bdf1238119994e868b455404004aaa4b8e5f78
elchinomandaron <133828359+elchinomandaron@users.noreply.github.com>
2023-12-17 21:54:54 -0600

    Actualizar SaludosBot.js

--- SaludosBot.js
+++ SaludosBot.js
@@ -2,5 +2,9 @@
   "¡Hola un gusto verte %UserName !",
   "¡Hola!",
   "¡Hola! Estoy Preparado para lo que me pidas!",
-  "¡Hola! Estoy Muy Emocionado!"
+  "¡Hola! Estoy Muy Emocionado!",
+  "¡Hola! que se te ofrece? %UserName",
+  "¡Hola! un gusto mi %UserName",
+  "¡Hola! %UserName que se te ofrece?",
+  "¡Hola como andas mi %UserName!",
 ];
