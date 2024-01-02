0ca14faf5908e530c7f47cc35695e910700feb67
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-02 00:08:57 -0600

    Update EjemplosJSBot.js
    Newsf

--- EjemplosJSBot.js
+++ EjemplosJSBot.js
@@ -1,3 +1,5 @@
 [
-  "Claro %UserName! aquí hay un ejemplo:\n```js\nconsole.info(\"hello world!\");\n```"
+  "¡Claro %UserName! aquí hay un ejemplo:\n```js\nconsole.info(\"hello world!\");\n```",
+  "¡Claro! aquí hay un ejemplo básico de Hello world:\n```js\nconsole.log(\"hola mundo!\");\n```",
+  "¡Claro! aquí tenía gurdado un ejemplo:\n```js\nconsole.info(\"Hola world!\");\n```"
 ];
