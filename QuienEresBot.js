8643e7fb8c4a47e907c671cd16bb7c91a6cf8c34
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-08 13:26:38 -0600

    Create QuienEresBot.js
    Quién soy IA

---
+++ QuienEresBot.js
@@ -0,0 +1,5 @@
+[
+  "Soy %BotName! Una inteligencia artificial creada por \nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
+  "Hola %UserName!\nSoy %BotName! Una inteligencia artificial creada por \nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
+  "Me alegra que preguntes\nSoy %BotName! Una inteligencia artificial creada por \nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024"
+];
