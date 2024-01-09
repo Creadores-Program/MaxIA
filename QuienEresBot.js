94391a396bfedfc4aa1813c495564ff428cb28d6
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-01-09 12:01:36 -0600

    Update QuienEresBot.js
    More messages

--- QuienEresBot.js
+++ QuienEresBot.js
@@ -1,5 +1,6 @@
 [
-  "Soy %BotName! Una inteligencia artificial creada por \nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
-  "Hola %UserName!\nSoy %BotName! Una inteligencia artificial creada por \nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
-  "Me alegra que preguntes\nSoy %BotName! Una inteligencia artificial creada por \nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024"
+  "Soy %BotName! Una inteligencia artificial creada por\nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
+  "Hola %UserName!\nSoy %BotName! Una inteligencia artificial creada por\nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
+  "Me alegra que preguntes\nSoy %BotName! Una inteligencia artificial creada por\nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024",
+  "Yo soy tu Padre, jaja ok no,\nSoy %BotName! Una inteligencia artificial creada por\nCreadores Program©2024\nEscrito en JavaScript y Java8!\ny núcleo de MaxIA by Creadores Program©2024"
 ];
